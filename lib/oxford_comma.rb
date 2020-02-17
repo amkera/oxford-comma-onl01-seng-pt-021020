7fbb7e4e775dab8866841aec2e92da2ece1c880b
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-17 06:59:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,14 +2,17 @@ def oxford_comma(array)
   counter = 0 
   new_array = []
   while counter < array.length - 1
-    new_array.push(array[counter])
+    new_array.push(" " + array[counter])
     counter += 1
   end
-  new_array.push("and #{array.last}")
+  new_array.push(" and #{array.last}")
   new_string = new_array.join(",")
   return new_string
 end
 
 oxford_comma(["fiddleheads","okra","kohlrabi"])
+
+
+
 
 
