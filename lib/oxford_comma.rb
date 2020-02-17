f95a4f799df25ff05c66237b08aa63956fab2239
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-17 07:15:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,12 @@
 def oxford_comma(array)
-  counter = 0 
-  new_array = []
-  while counter < array.length - 1
-    new_array.push(" " + array[counter])
-    counter += 1
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(", ") + ", and " + array[-1]
   end
-  new_array.push(" and #{array.last}")
-  new_string = new_array.join(",")
-  return new_string
 end
 
-oxford_comma(["fiddleheads","okra","kohlrabi"])
-
-
-
-
+oxford_comma(['anteater', 'bear', 'crocodile', 'dingo', 'elephant', 'ferret'])
 
